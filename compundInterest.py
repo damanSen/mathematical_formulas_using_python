f2587c22da4d6b3911c38874589caca625afe21f
daman <92129992+damanSen@users.noreply.github.com>
2021-12-24 10:04:01 -0500

    Add files via upload

---
+++ compundInterest.py
@@ -0,0 +1,11 @@
+def compound_interest(p, t ,r):
+    print(f"The principal is {p}")
+    print(f"The time period is {t}")
+    print(f"The interest rate is {r}")
+
+    ci=p*(1+r/100)**t
+
+    print(f"The compounded interest is {ci}")
+    return ci
+
+compound_interest(2500, 10, 5)
